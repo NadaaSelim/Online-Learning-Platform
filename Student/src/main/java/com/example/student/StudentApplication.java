bb6aa158176839dc357210e292785aba28befd33
NadaaSelim <nadaselim3333@gmail.com>
2024-05-16 18:20:37 +0300

    enrollment service

--- Student/src/main/java/com/example/student/StudentApplication.java
+++ Student/src/main/java/com/example/student/StudentApplication.java
@@ -4,12 +4,14 @@ package com.example.student;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class StudentApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentApplication.class, args);
